d3685db7fbe6f0a0fb18ab02c998df465517c731
weljim73-spec <weljim73@gmail.com>
2026-01-28 22:16:13 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="Mia Training Tracker",
     page_icon="⚽",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 import pandas as pd
